c6c998b5eb1d037fd4349891d054cfcdf015b953
Sayon Usman <sayonusman.ma@gmail.com>
2023-05-03 12:21:57 +0300

    Fixed margin problem of food category card

--- src/pages/FoodCategories/FoodCategories.jsx
+++ src/pages/FoodCategories/FoodCategories.jsx
@@ -7,7 +7,7 @@ const FoodCategories = () => {
 
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 m-2">
         {categories.map((category) => (
           <FoodCategoryCard
             key={category.id}
