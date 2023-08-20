5180ad4ec792d473764e56f2dd25f4593ed1b0f1
Sayon Usman <sayonusman.ma@gmail.com>
2023-08-20 22:52:23 +0300

    Error page has been updated

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 const ErrorPage = () => {
   return (
     <div className="hero min-h-screen">
-      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <main className="grid min-h-full place-items-center bg-[#2a303c] text-white px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-          <p className="text-base font-semibold text-indigo-600">404</p>
+          <p className="text-base font-semibol">404</p>
 
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+          <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
             Page not found
           </h1>
 
-          <p className="mt-6 text-base leading-7 text-gray-600">
+          <p className="mt-6 text-base leading-7">
             Sorry, we couldn’t find the page you’re looking for.
           </p>
 
